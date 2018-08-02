3c7f2dbc87d7413de4c724078f61f3650a8a2d32
massa <massafranck@gmail.com>
2018-08-02 23:18:37 +0200

    corrigé

--- index.php
+++ index.php
@@ -1,18 +1,17 @@
+<?php
+ ?>
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-  <meta charset="utf-8" />
-  <title>Exercice6</title>
-</head>
-<body>
-  <p>
-    <?php
-    //Déclaration de la variable haut De France avec pour index le numéro des départements
-    $hautDeFrance = array('60' => 'Oise', '02' => 'Aisne',
-    '80' => 'Somme', '59' => 'Nord', '62' => 'Pas-De-Calais');
-    //Affichage de la valeur de l'index 59
-    echo $hautDeFrance [59];
-    ?>
-  </p>
-</body>
+  <head>
+    <meta charset="utf-8" />
+    <title>p5exo6</title>
+  </head>
+  <body>
+    <p>
+      <?php
+        $departements = array('02' => 'Aisne','59' => 'Nord', '60' => 'Oise', '80' => 'Somme', '62' => 'Pas-de-Calais');
+          echo $departements['59'];
+      ?>
+    </p>
+  </body>
 </html>
